977988588336429a1f2fa18237302826f99524b1
Ananya Ananth <ananyaananth29@gmail.com>
2022-03-22 13:13:35 +0530

    fifth output

--- src/components/user/Card.js
+++ src/components/user/Card.js
@@ -1,19 +1,29 @@
 // components/user/Card.js
-import React  from "react";
+import React from "react";
 import { Text } from "./Text";
 import { Button } from "./Button";
 import { Container } from "./Container";
+import { useNode, Element } from "@craftjs/core";
 
-export const Card = ({background, padding = 20}) => {
+
+export const Card = ({ background, padding = 20 }) => {
   return (
     <Container background={background} padding={padding}>
-      <div className="text-only">
+      <Element id="text" canvas>
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />
-      </div>
-      <div className="buttons-only">
-        <Button size="small" text="Learn more" variant="contained" color="primary" />
-      </div>
+      </Element>
+
+      <Element id="buttons" canvas>
+        {" "}
+        // Canvas Node of type div
+        <Button
+          size="small"
+          text="Learn more"
+          variant="contained"
+          color="primary"
+        />
+      </Element>
     </Container>
-  )
-}
+  );
+};
