2be866841959378df51d87a6571e766a43cebc00
Ananya Ananth <ananyaananth29@gmail.com>
2022-03-22 12:31:14 +0530

    second output

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 // pages/index.js
-
 import React from 'react';
 import {Typography, Paper, Grid} from '@material-ui/core';
 
 import { Toolbox } from './components/Toolbox';
 import { SettingsPanel } from './components/SettingsPanel';
-import { Topbar } from './components/Topbar';
 
 import { Container } from './components/user/Container';
 import { Button } from './components/user/Button';
 import { Card } from './components/user/Card';
 import { Text } from './components/user/Text';
 
+import {Editor, Frame, Element} from "@craftjs/core";
+
 export default function App() {
+  const classes=()=>{}
   return (
-    <div style={{margin: "0 auto", width: "800px"}}>
+    <div>
       <Typography variant="h5" align="center">A super simple page editor</Typography>
-      <Grid container spacing={3} style={{paddingTop: "10px"}}>
-        <Topbar />
-        <Grid item xs>
-          <Container padding={5} background="#eee">
-            <Card />
-          </Container>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>
-              <Toolbox />
-              <SettingsPanel />
-          </Paper>          
-        </Grid>
-      </Grid>
+        <Editor resolver={{Card, Button, Text, Container}}> 
+          <Grid container spacing={3}>
+            <Grid item xs>
+              <Frame>
+                <Container padding={5} background="#eee">
+                  <Card />
+                  <Button size="small" variant="outlined">Click</Button>
+                  <Text size="small" text="Hi world!" />
+                  <Container padding={6} background="#999">
+                    <Text size="small" text="It's me again!" />
+                  </Container>
+                </Container>
+              </Frame>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.root}>
+                  <Toolbox />
+                  <SettingsPanel />
+              </Paper>          
+            </Grid>
+          </Grid>
+        </Editor>
     </div>
   );
 }
